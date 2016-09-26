f1a0680365ead18da9be7e0647ec68c3152d69ef
WasifaChowdhury <wasifa.chy@gmail.com>
2016-09-25 17:13:39 -0800

    updated

--- UnigramModelSegmentation.py
+++ UnigramModelSegmentation.py
@@ -1,4 +1,4 @@
-﻿# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import sys, codecs, optparse, os, operator, math, numpy as np
 
@@ -31,11 +31,7 @@ class Pdist(dict):
 		self.missingfn = missingfn or (lambda k, N: 1./N)
 
 	def __call__(self, key):
-		# default algorithm didn't use call method
-		# float(self[key])/float(self.N) returns the probability (freq of each unigram word/total freq of all words in file) for each key (e.g. unigram word in file) in dictionary
 		if key in self: return float(self[key])/float(self.N)
-		#else: return self.missingfn(key, self.N)
-		# else if the word consists of only a single character (e.g. len=1), then return missingfn=1.0/N (the character occured only once/total frequency of all words in file)
 		elif len(key) == 1: return self.missingfn(key, self.N)
 		else: return None
 
@@ -46,9 +42,28 @@ class Entry(object):
 		self.logprob = logprob
 		self.backpointer = backpointer
 
-# the default segmenter does not use any probabilities, but you could ...
+def checkUnigram(utf8line, newindex, objentry, prob):
+    for newword in list_sorted:
+        if newword[0] in utf8line and utf8line.startswith(newword[0], newindex, newindex + len(newword[0])):
+            newentry = Entry(newword[0], newindex, np.sum([prob, newword[2]]), objentry)
+            if newentry not in heap:
+                heap.append(newentry)
+
+def checkSingleWord(newindex, oneword, objentry, prob):
+    if newindex <= len(oneword)-1 and oneword[newindex] not in heap:
+        heap.append(Entry(oneword[newindex], newindex, np.sum([prob, np.log2(Pw.__call__(oneword[newindex]))]), objentry))
+
+def bestSeg(utf8line, chart):
+    finalindex = len(utf8line) - 1
+    entry = chart[finalindex]
+    bestsegmentation = []
+    while entry is not None:
+        bestsegmentation.append(entry.word)
+        entry = entry.backpointer
+    bestsegmentation.reverse()
+    print " ".join(bestsegmentation)
+
 Pw  = Pdist(opts.counts1w)
-# old = sys.stdout
 sys.stdout = codecs.lookup('utf-8')[-1](sys.stdout)
 list1 = []
 for line in file(opts.counts1w):
@@ -56,7 +71,6 @@ for line in file(opts.counts1w):
 list = [line.split() for line in list1]
 for line in list:
     line.append(np.log2(Pw.__call__(line[0])))
-# unigrams sorted according to logprob in descending order
 # list_sorted is a sorted list of lists where each entry=list=[word,freq,prob]
 list_sorted = sorted(list, key=lambda prob: prob[2], reverse=True)
 
@@ -68,12 +82,8 @@ with open(opts.input) as f:
       chart = [None] * len(utf8line)
       # oneword contain words which are 1 character long, and assign a small freq=1/N to them
       oneword = [i for i in utf8line]
-      # for each word in list_sorted that matches input at position 0
-      for entry in list_sorted:
-          if entry[0] in utf8line and utf8line.startswith(entry[0], 0, len(entry[0])):
-              heap.append(Entry(entry[0], 0, entry[2], None))
-      if oneword[0] not in heap:
-          heap.append(Entry(oneword[0], 0, np.log2(Pw.__call__(oneword[0])), None))
+      checkUnigram(utf8line, 0, None, 0)
+      checkSingleWord(0, oneword, None, 0)
       while len(heap)!=0:
           objentry = heap[0]
           word = heap[0].word
@@ -89,20 +99,6 @@ with open(opts.input) as f:
           else:
               chart[endindex] = objentry
           newindex = endindex + 1
-          for newword in list_sorted:
-                if newword[0] in utf8line and utf8line.startswith(newword[0], newindex, newindex + len(newword[0])):
-                        newentry = Entry(newword[0], newindex, np.sum([prob, newword[2]]), objentry)
-                        if newentry not in heap:
-                            heap.append(newentry)
-          if newindex <= len(oneword)-1 and oneword[newindex] not in heap:
-              heap.append(Entry(oneword[newindex], newindex, np.sum([prob, np.log2(Pw.__call__(oneword[newindex]))]), objentry))
-
-      finalindex = len(utf8line)-1
-      entry = chart[finalindex]
-      bestsegmentation = []
-      while entry is not None:
-          bestsegmentation.append(entry.word)
-          entry = entry.backpointer
-      bestsegmentation.reverse()
-      print " ".join(bestsegmentation)
-# sys.stdout = old
+          checkUnigram(utf8line, newindex, objentry, prob)
+          checkSingleWord(newindex, oneword, objentry, prob)
+      bestSeg(utf8line, chart)
