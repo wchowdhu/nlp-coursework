9385cc1a32887d69bdadda3375da258e95a19e54
WasifaChowdhury <wasifa.chy@gmail.com>
2016-11-08 14:16:43 -0800

    Update baseline_beamwidth.py

--- baseline_beamwidth.py
+++ baseline_beamwidth.py
@@ -15,7 +15,7 @@ def find_beam(stack):
     min = beam[-1].logprob
     # beam_width = (max + min)/2.0
     # beam_filtered = [hypothesis for hypothesis in beam if hypothesis.logprob >= beam_width]
-    beam_width = log10(0.00000001)
+    beam_width = log10(0.000000001)
     beam_filtered = [hypothesis     for hypothesis in beam     if hypothesis.logprob >= (max + beam_width)]
     # beam_filtered = [hypothesis     for hypothesis in beam      if abs(max - hypothesis.logprob) <= abs(beam_width)]
     # returns list of sorted hypothesis/states according to logprob and filtered according to beam width
